6cba7cd2daa28ef8e64e613dbed6c575a2187c19
pemeraldy <pemeraldy@gmail.com>
2020-12-19 12:53:33 +0100

    background color changed

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
+    <v-app-bar flat app color="primary" dark>
+      <v-icon color="success">mdi-home</v-icon>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-tabs>
+        <v-spacer></v-spacer>
+        <v-tab class="green--text">
+          <v-icon class="white">mdi-open-in-new</v-icon>
+          Item One
+        </v-tab>
+        <v-tab class="green--text text--lighten-1">Item Two</v-tab>
+        <v-tab class="orange--text">Item Three</v-tab>
+      </v-tabs>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
@@ -56,5 +34,10 @@ export default {
   data: () => ({
     //
   }),
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    },
+  },
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,7 +2,31 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
-
+// let darkBlue = 
+// {
+//     primary: '#1976D2',
+        // cardBackground: '#1b2635',
+//     secondary: '#424242',
+//     accent: '#82B1FF',
+//     error: '#FF5252',
+//     info: '#2196F3',
+//     success: '#4CAF50',
+//     warning: '#FFC107',
+//   }
 export default new Vuetify({
-    theme: { dark: true },
+    theme: {
+        dark: true,
+        themes: {
+          dark: {
+            background: '#0a192f',
+            primary: '#0a192f',
+            accent: '#233044',
+            success: '#64ffda'
+          },
+          light: {
+            background: '#d0f0c0'
+          }
+        }
+      }
+
 });
