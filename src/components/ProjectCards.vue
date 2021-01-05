4ee3b24dd6347e5bf7913c11276cde4d2e3d1ad2
pemeraldy <pemeraldy@gmail.com>
2021-01-05 14:24:01 +0100

    projects section component created and data shared between

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,29 +1,65 @@
 <template>
-  <v-card class="mx-auto backgroundSecondary" max-width="400">
+  <v-card
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    class="mx-auto backgroundSecondary project-card"
+    max-width="344"
+    min-height="450"
+    max-height="450"
+  >
     <v-img
       class="white--text align-end"
-      height="200px"
-      src="/images/demo/cards/docks.jpg"
+      height="300px"
+      :src="projectDetails.img"
     >
-      <v-card-title>Top 10 Australian beaches</v-card-title>
+      <v-card-title class="primaryText--text">{{
+        projectDetails.title
+      }}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+    <!-- <v-card-subtitle class="pb-0">Number 10</v-card-subtitle> -->
 
     <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
+      <div class="text-truncate">{{ projectDetails.desc }}</div>
 
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <!-- <div>Whitsunday Island, Whitsunday Islands</div> -->
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" text>
-        Share
-      </v-btn>
-
-      <v-btn color="orange" text>
-        Explore
+      <v-btn
+        class="success--text"
+        :href="projectDetails.link"
+        target="_blank"
+        color="success"
+        text
+      >
+        View live
       </v-btn>
     </v-card-actions>
+    <!-- <v-overlay absolute="true" overlay="hover"></v-overlay> -->
   </v-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  props: {
+    projectDetails: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.project-card {
+  transition: 0.3s all ease-in;
+}
+.project-card:hover {
+  transform: scale(1.05);
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,26 +13,16 @@
           </span>
         </div>
         <div class="dispplay-1 primaryText--text my-10">
-          Below is a collection of personal projects I have developed .
+          Below is a gallery of personal projects I built at my leisure time .
         </div>
         <!-- <v-card class="mx-auto"> -->
         <v-container>
-          <h1>Projects comes here</h1>
           <v-row dense>
-            <v-col mb="5">
-              <project-cards />
-            </v-col>
-            <v-col mb="5">
-              <project-cards />
-            </v-col>
-            <v-col mb="5">
-              <project-cards />
-            </v-col>
-            <v-col mb="5">
-              <project-cards />
-            </v-col>
-            <v-col mb="5">
-              <project-cards />
+            <v-col mb="5" v-for="project in projects" :key="project.title">
+              <h3 class="text-center mb-3 primaryText--text">
+                {{ project.title }}
+              </h3>
+              <project-cards :projectDetails="project" />
             </v-col>
           </v-row>
         </v-container>
@@ -46,14 +36,30 @@
 import ProjectCards from "./ProjectCards.vue";
 export default {
   components: { ProjectCards },
-  name: "AboutApp",
+  name: "Projects",
   data: () => ({
-    skills: [
-      { text: "Javascript", icon: "mdi-language-javascript" },
-      { text: "Vue", icon: "mdi-vuejs" },
-      { text: "React", icon: "mdi-react" },
-      { text: "HTML & CSS", icon: "mdi-language-html5" },
-      { text: "Nodejs", icon: "mdi-nodejs" },
+    projects: [
+      {
+        img: require("../assets/projects/yopix.jpeg"),
+        title: "Yopix",
+        desc:
+          "A pexel clone built using pexel Api and Vuejs. It displays a grid of nice shots taking by awesome photographers.",
+        link: "https://yopix.netlify.app/",
+      },
+      {
+        img: require("../assets/projects/movie-share-min.jpeg"),
+        title: "Mo-Vie-Share",
+        desc:
+          "A movie collection app, where registered users can create a collection of movie list and share with the world",
+        link: "https://movie-share.netlify.app/",
+      },
+      {
+        img: require("../assets/projects/yopee-store-min.jpeg"),
+        title: "Yopee Store",
+        desc:
+          "An ecommerce website built using Nodejs and Vuejs. It has the basic features an ecommerce site: Add to cart, check out page and payment page",
+        link: "https://yopee-store.netlify.app/",
+      },
     ],
   }),
 };
