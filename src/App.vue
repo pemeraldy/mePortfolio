a8710ffb3a031b949beb07a56ceabc3f97c4f05f
pemeraldy <pemeraldy@gmail.com>
2021-01-05 15:13:06 +0100

    added a drawer to nav

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
     <v-app-bar app color="background " class="px-4" dark>
-      <v-icon class="d-block d-sm-none" color="success">mdi-home</v-icon>
+      <v-icon
+        @click="drawer = !drawer"
+        class="d-block d-sm-none"
+        color="success"
+        >mdi-apps-box</v-icon
+      >
 
       <v-tabs color="success" class="px-3 d-none d-sm-block">
         <v-spacer></v-spacer>
         <v-tab class="success--text ">About</v-tab>
         <v-tab class="success--text">Experience</v-tab>
-        <v-tab class="success--text">Work</v-tab>
+        <v-tab class="success--text">Projects</v-tab>
       </v-tabs>
     </v-app-bar>
+    <v-navigation-drawer color="background" v-model="drawer" absolute temporary>
+      <div class="mb-2"></div>
+      <v-spacer></v-spacer>
+      <v-list-item>
+        <v-list-item-avatar>
+          <!-- <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img> -->
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="success--text">Peter Olu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container>
@@ -45,7 +77,13 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-view-user" },
+      { title: "About", icon: "mdi-user" },
+      { title: "Experience", icon: "mdi-user" },
+      { title: "Projects", icon: "mdi-user" },
+    ],
   }),
   computed: {
     theme() {
